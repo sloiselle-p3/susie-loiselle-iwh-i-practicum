58bbd18116a5bb0f4ef0f080727c1e6068148015
Susie Loiselle <sloiselle@parkour3.com>
2026-05-03 22:52:33 -0400

    token added outside tracked files

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
 const app = express();
@@ -7,12 +8,28 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+//const CLIENT_SECRET = process.env.clientSecret;
+//const REDIRECT_URL = `http://localhost:3000/oauth-callback`;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// *DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account
+const PRIVATE_APP_TOKEN = process.env.clientID;
 
-// * Code for Route 1 goes here
+// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. 
+// Pass this data along to the front-end and create a new pug template in the views folder.
+app.get('/contacts', async (req, res) => {
+    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
+        'Content-Type': 'application/json'
+    }
+    try {
+        const resp = await axios.get(contacts, { headers });
+        const data = resp.data.results;
+        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
@@ -29,7 +46,7 @@ const PRIVATE_APP_ACCESS = '';
 app.get('/contacts', async (req, res) => {
     const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
         'Content-Type': 'application/json'
     }
     try {
@@ -52,7 +69,7 @@ app.post('/update', async (req, res) => {
     const email = req.query.email;
     const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
     const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        Authorization: `Bearer ${PRIVATE_APP_TOKEN}`,
         'Content-Type': 'application/json'
     };
 
